3ac79a5e1e09c29138cfa2c143b8499aaffd2624
Xandowski <xandy-moraes@hotmail.com>
2019-04-28 23:28:11 -0300

    Desafio-08

--- DesafiosJavaScriptNinja/desafio-08.js
+++ DesafiosJavaScriptNinja/desafio-08.js
@@ -3,20 +3,24 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-let sum = (x, y) => {return x + y};
+let sum = function calculateSum(x, y){
+    return x + y;
+};
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-console.log(sum(5, 2));
+let value1 = 5
+let value2 = 15
+console.log('A soma de ' + value1 + ' e ' + value2 + ' é ' + sum(value1, value2));
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-console.log(sum.name);
+console.log('O nome da função que faz a soma é ' + sum.name);
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
