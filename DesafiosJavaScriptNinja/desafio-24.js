3d2c3a5ad42512d0aef898a43bc3dc4e2a4a9469
Xandowski <xandy-moraes@hotmail.com>
2019-05-19 20:48:49 -0300

    Desafio-24-Modularização-do-codigo

---
+++ DesafiosJavaScriptNinja/desafio-24.js
@@ -0,0 +1,104 @@
+(function (win, doc) {
+  "use strict";
+  /*
+  Nossa calculadora agora está funcional!A ideia desse desafio é modularizar
+  o código, conforme vimos na aula anterior.Quebrar as responsabilidades
+  em funções, onde cada função faça somente uma única coisa, e faça bem feito. -
+    Remova as duplicações de código; -
+  agrupe os códigos que estão soltos em funções(declarações de variáveis,
+    listeners de eventos, etc); -
+  faça refactories para melhorar esse código, mas de forma que o mantenha com a
+  mesma funcionalidade.
+  */
+ 
+  const $visor = doc.querySelector('[data-js="visor"]');
+  const $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+  const $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
+  const $buttonCE = doc.querySelector('[data-js="button-ce"]');
+  const $buttonEqual = doc.querySelector('[data-js="button-equal"]');
+
+  function initialize() {
+    initEvents();
+  }
+
+  function initEvents() {
+    Array.prototype.forEach.call($buttonsNumbers, (button) => {
+      button.addEventListener('click', handleClickNumber, false);
+    });
+    Array.prototype.forEach.call($buttonsOperations, (button) => {
+      button.addEventListener('click', handleClickOperation, false);
+    });
+    $buttonCE.addEventListener('click', handleClickCE, false);
+    $buttonEqual.addEventListener('click', handleClickEqual, false);
+  }
+
+  function handleClickNumber() {
+    $visor.value += this.value;
+  }
+
+  function handleClickOperation() {
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
+    $visor.value += this.value;
+  }
+
+  function handleClickCE() {
+    $visor.value = 0;
+  }
+
+  function isLastItemAnOperation(number) {
+    const operations = getOperations();
+    const lastItem = number.split('').pop();
+    return operations.some((operator) => {
+      return operator === lastItem;
+    });
+  }
+
+  function getOperations() {
+    return Array.prototype.map.call($buttonsOperations, (button) => {
+      return button.value;
+    });
+  }
+
+  function removeLastItemIfItIsAnOperator(string) {
+    if (isLastItemAnOperation(string))
+      return string.slice(0, -1);
+    return string;
+  }
+
+  function handleClickEqual() {
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
+    const allValues = $visor.value.match(getRegexOperations());
+    $visor.value = allValues.reduce(calculateAllValues);
+  }
+
+  function getRegexOperations() {
+    return new RegExp('\\d+[' + getOperations().join('') + ']?', 'g');
+  }
+
+  function calculateAllValues(accumulated, actual) {
+    const firstValue = accumulated.slice(0, -1);
+    const operator = accumulated.split('').pop();
+    const lastValue = removeLastItemIfItIsAnOperator(actual);
+    const lastOperator = getLastOperator(actual);
+    return doOperation(operator, firstValue, lastValue) + lastOperator;
+  }
+
+  function getLastOperator(value) {
+    return isLastItemAnOperation(value) ? value.split('').pop() : '';
+  }
+
+  function doOperation(operator, firstValue, lastValue) {
+    switch (operator) {
+      case '+':
+        return Number(firstValue) + Number(lastValue);
+      case '-':
+        return Number(firstValue) - Number(lastValue);
+      case 'x':
+        return Number(firstValue) * Number(lastValue);
+      case '÷':
+        return Number(firstValue) / Number(lastValue);
+    }
+  }
+
+  initialize();
+})(window, document);
