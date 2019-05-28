5b490e2d35dcff87d67369d40184d3453930c9e6
Xandowski <xandy-moraes@hotmail.com>
2019-05-28 15:33:24 -0300

    Desafio-28-Consultar-CEP-update

--- DesafiosJavaScriptNinja/desafio-28.js
+++ DesafiosJavaScriptNinja/desafio-28.js
@@ -1,4 +1,4 @@
-(function (win, doc) {
+(function (DOM) {
   "use strict";
   /*
   No HTML:
@@ -27,85 +27,6 @@
   adicionar as informações em tela.
   */
 
-  function DOM(elements) {
-    this.element = doc.querySelectorAll(elements);
-  }
-
-  DOM.prototype.on = function on(eventType, callback) {
-    Array.prototype.forEach.call(this.element, (element) => {
-      element.addEventListener(eventType, callback, false);
-    });
-  };
-
-  DOM.prototype.off = function off(eventType, callback) {
-    Array.prototype.forEach.call(this.element, (element) => {
-      element.removeEventListener(eventType, callback, false);
-    });
-  };
-
-  DOM.prototype.get = function get() {
-    return this.element;
-  };
-
-
-  DOM.prototype.forEach = function forEach() {
-    return Array.prototype.forEach.apply(this.element, arguments);
-  };
-
-  DOM.prototype.map = function map() {
-    return Array.prototype.map.apply(this.element, arguments);
-  };
-
-  DOM.prototype.filter = function filter() {
-    return Array.prototype.filter.apply(this.element, arguments);
-  };
-
-  DOM.prototype.reduce = function reduce() {
-    return Array.prototype.reduce.apply(this.element, arguments);
-  };
-
-  DOM.prototype.reduceRight = function reduceRight() {
-    return Array.prototype.reduceRight.apply(this.element, arguments);
-  };
-
-  DOM.prototype.every = function every() {
-    return Array.prototype.every.apply(this.element, arguments);
-  };
-
-  DOM.prototype.some = function some() {
-    return Array.prototype.some.apply(this.element, arguments);
-  };
-
-
-  DOM.prototype.isArray = function isArray(param) {
-    return Object.prototype.toString.call(param) === '[object Array]';
-  };
-
-  DOM.prototype.isObject = function isObject(param) {
-    return Object.prototype.toString.call(param) === '[object Object]';
-  };
-
-  DOM.prototype.isFunction = function isFunction(param) {
-    return Object.prototype.toString.call(param) === '[object Function]';
-  };
-
-  DOM.prototype.isNumber = function isNumber(param) {
-    return Object.prototype.toString.call(param) === '[object Number]';
-  };
-
-  DOM.prototype.isString = function isString(param) {
-    return Object.prototype.toString.call(param) === '[object String]';
-  };
-
-  DOM.prototype.isBoolean = function isBoolean(param) {
-    return Object.prototype.toString.call(param) === '[object Boolean]';
-  };
-
-  DOM.prototype.isNull = function isObject(param) {
-    return Object.prototype.toString.call(param) === '[object Null]' ||
-      Object.prototype.toString.call(param) === '[object Undefined]';
-  };
-  
   const $form = new DOM('[data-js="form"]');
   const $inputCEP = new DOM('[data-js="input-cep"]');
   const $logradouro = new DOM('[data-js="logradouro"]');
@@ -192,4 +113,4 @@
     return message.replace('[CEP]', clearCEP());
   }
 
-})(window, document);
+})(window.DOM);
